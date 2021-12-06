e3da5ed63ca9a1aa23d70835332b06dead49526b
NGUYENFrancois <nguyenfrancois78280@gmail.com>
2021-12-06 10:40:02 +0100

    initialisation

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrateur
+#
+# Created:     06/12/2021
+# Copyright:   (c) Administrateur 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+str = "! Hello World !"
+print("\n",str)
