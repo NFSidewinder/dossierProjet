ed6c51d1375081f2ad52ae33031b1285074f9812
NGUYENFrancois <nguyenfrancois78280@gmail.com>
2021-12-06 11:52:26 +0100

    ajout de '! Bonjour Monde '

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,7 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Administrateur
-#
-# Created:     06/12/2021
-# Copyright:   (c) Administrateur 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
 str = "! Hello World !"
+print("\n",str)
+
+str = "! Bonjour Monde !"
 print("\n",str)
